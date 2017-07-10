ef04ef80c066f5cef8acd90b38289141b38d36e9
Michael Axelson <axelmichael11@gmail.com>
2017-07-10 14:57:53 -0700

    burger routes are good

---
+++ route/burger-router.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const {Router} = require('express');
+
+const s3Upload = require('../lib/s3-upload-middleware.js');
+const bearerAuth = require('../lib/bearer-auth-middleware.js');
+const Burger = require('../model/burger.js');
+const bodyParser = require('body-parser').json();
+
+const burgerRouter = module.exports = new Router();
+
+burgerRouter.post('/api/burgers', bearerAuth, s3Upload('image'), (req,res, next) => {
+  console.log('hitting post /api/burgers route');
+  new Burger({
+    name: req.body.name,
+    rating: req.body.rating,
+    price: req.body.price,
+    flavor_profile: req.body.flavor_profile,
+    description: req.body.description,
+    photo_URL: req.s3Data.Location,
+    veggie: req.body.veggie,
+  })
+    .save()
+    .then(burger => res.json(burger))
+    .catch(next);
+});
+
+
+burgerRouter.get('/api/burgers:id', (req, res, next) => {
+  console.log('hit get /api/bars/:id');
+  Burger.findById(req.params.id)
+    .then(bar => {
+      res.json(bar);
+    })
+    .catch(next);
+});
+
+burgerRouter.put('/api/burgers/:id', bodyParser, (req, res, next) => {
+  let options = {
+    runValidators: true,
+    new: true,
+  };
+  Burger.findByIdAndUpdate(req.params.id, req.body, options)
+    .then(restaurant => res.json(restaurant))
+    .catch(next);
+});
+
+burgerRouter.delete('/api/burgers/:id', (req,res,next) => {
+  Burger.findByIdAndRemove(req.params.id)
+    .find({})
+    .then(() => res.sendStatus(204))
+    .catch(next);
+});
