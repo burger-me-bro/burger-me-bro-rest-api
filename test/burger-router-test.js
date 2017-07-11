bb8e490e2af92a20b71f51c94ee89ed0b3f6b064
Joshua Evans <skyfriends1@gmail.com>
2017-07-11 12:25:53 -0700

    removed unused mockburger

--- test/burger-router-test.js
+++ test/burger-router-test.js
@@ -5,9 +5,7 @@ require('dotenv').config({ path: `${__dirname}/../.test.env` });
 
 const expect = require('expect');
 const superagent = require('superagent');
-const mockBurger = require('./lib/mock-burger.js');
 const mockUser = require('./lib/mock-user.js');
-
 const server = require('../lib/server.js');
 const cleanDB = require('./lib/clean-db.js');
 require('./lib/aws-mock.js');
@@ -20,27 +18,27 @@ describe('testing burger router', () => {
   afterEach(cleanDB);
 
   describe('testing POST /api/burgers', () => {
-    // it('should return a 200', () => {
-    //   let testUser;
-    //   return mockUser.createOne()
-    //     .then(userData => {
-    //       testUser = userData.user;
-    //       console.log(testUser);
-    //       return superagent.post(`${API_URL}/api/burgers`)
-    //         .set('Authorization',  `Bearer ${userData.token}`)
-    //         .field('name', 'test_burgerrrr')
-    //         .field('rating', 'good')
-    //         .field('price', 5)
-    //         .field('flavor_profile', 'tangy')
-    //         .field('description', 'so good!')
-    //         .field('veggie', false)
-    //         .attach('image', `${__dirname}/assets/burger.jpg`);
-    //     })
-    //     .then(res => {
-    //       console.log(res);
-    //       expect(res.body).toExist();
-    //     });
-    // });
+    it('should return a 200', () => {
+      let testUser;
+      return mockUser.createOne()
+        .then(userData => {
+          testUser = userData.user;
+          console.log(testUser);
+          return superagent.post(`${API_URL}/api/burgers`)
+            .set('Authorization',  `Bearer ${userData.token}`)
+            .field('name', 'test_burgerrrr')
+            .field('rating', 'good')
+            .field('price', 5)
+            .field('flavor_profile', 'tangy')
+            .field('description', 'so good!')
+            .field('veggie', false)
+            .attach('image', `${__dirname}/assets/burger.jpg`);
+        })
+        .then(res => {
+          console.log(res);
+          expect(res.body).toExist();
+        });
+    });
     it('should return a 400 for a bad request', () => {
       let testUser;
       return mockUser.createOne()
