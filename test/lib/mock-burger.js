9546b8f399907a53f46a14aa9353ccca8dd943b8
Michael Axelson <axelmichael11@gmail.com>
2017-07-10 17:01:36 -0700

    mock burger is now generating random review info..

--- test/lib/mock-burger.js
+++ test/lib/mock-burger.js
@@ -6,6 +6,23 @@ const Burger = require('../../model/burger.js');
 
 const mockBurger = module.exports = {};
 
+mockBurger.randomBurgerProfile = () => {
+  let flavors = ['tangy','sweet','spicy','funky','crunchy','savory','greasy'];
+  return flavors[Math.floor(Math.random()*flavors.length)];
+};
+
+mockBurger.randomRating = () => {
+  let ratings = ['really bad','bad','average','good','really good'];
+  return ratings[Math.floor(Math.random()*ratings.length)];
+};
+
+
+mockBurger.randomPrice = () => {
+  return Math.floor(Math.random()*20);
+};
+
+
+
 mockBurger.createOne = () => {
   let result = {};
   return mockUser.createOne()
@@ -13,9 +30,9 @@ mockBurger.createOne = () => {
       result.user = user;
       return new Burger({
         name: faker.internet.userName(),
-        rating: 'good',
-        price: faker.random.number(),
-        flavor_profile: 'sweet',
+        rating: mockBurger.randomRating(),
+        price: mockBurger.randomPrice(),
+        flavor_profile: mockBurger.randomBurgerProfile(),
         description: faker.lorem.sentence(),
         photo_URL: faker.image.imageUrl(),
         veggie: false,
